0ee1892d38bddbe97b821a0ba34bee831be6dc0d
takayuki.kayawari <kayamelo@gmail.com>
2020-01-04 19:48:19 +0900

    fix route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   use_doorkeeper
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
   namespace :api do
     namespace :v1 do
-      get '/me' => 'user#me'
+      get '/me' => 'users#me'
     end
   end
   root to: 'home#show'
