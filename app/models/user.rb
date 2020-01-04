ea0e3d886593cf6cc4218f0d41311966625c407f
takayuki.kayawari <kayamelo@gmail.com>
2020-01-04 19:21:14 +0900

    Add new application

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable
 
   def update_uid!
     update(uid: id)
